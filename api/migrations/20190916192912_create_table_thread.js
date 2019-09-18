9a008f0b46b6a9e71cf1c21ed6ecc405e724bc8f
Pupperbot <pupperbot@protonmail.com>
2019-09-17 19:54:33 -0600

    Fix bug with comments not posting.

--- api/migrations/20190916192912_create_table_thread.js
+++ api/migrations/20190916192912_create_table_thread.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('thread_table', function(table) {
-      table.increments('thread_id');
+      table.string('thread_id').notNullable();
       table.string('title').notNullable();
       table.string('body').notNullable();
       table.string('date_created').notNullable();
-      table.string('user_created');
+      table.string('user_created').notNullable();
     })
 };
 
--- api/migrations/20190916194702_create_thread_comments.js
+++ api/migrations/20190916194702_create_thread_comments.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('thread_comments', function(table) {
-      table.increments('thread_id');
       table.string('comment_id').notNullable();
+      table.string('thread_id').notNullable();
       table.string('user_comment').notNullable();
       table.string('username').notNullable();
     })
