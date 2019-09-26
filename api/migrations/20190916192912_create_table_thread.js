39f97efed30812dffc6a9ed3b87d8bc181f59053
Pupperbot <pupperbot@protonmail.com>
2019-09-25 19:04:23 -0600

    Adjusted migration for extra data.

--- api/migrations/20190916192912_create_table_thread.js
+++ api/migrations/20190916192912_create_table_thread.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
    return knex.schema.createTable('thread_table', function(table) {
       table.string('thread_id').notNullable();
       table.string('title').notNullable();
-      table.string('body').notNullable();
+      table.string('body', 100000).notNullable();
       table.string('date_created').notNullable();
       table.string('user_created').notNullable();
     })
