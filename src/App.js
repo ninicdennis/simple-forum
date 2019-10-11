d1cb45f1afa0c87fa1ce58c75f4a5eb7ad477dd2
Pupperbot <pupperbot@protonmail.com>
2019-10-10 18:33:46 -0600

    Changed render method for Thread to pass Auth0 prop.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { Route } from "react-router-dom"
 import Homepage from './pages/homepage/homepage'
 import Userpage from './pages/userPage/userpage'
-import Topic from './pages/topicpage/topicpage'
+// import Topic from './pages/topicpage/topicpage'
 import CreateTopic from './pages/createtopic/createtopic'
 import Header from './component/header/header'
 import AboutMe from './pages/about/about';
+import AuthThread from './pages/topicpage/topic_auth';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Header />
       <Route exact path = '/' component = {Homepage}/>
       <Route exact path = '/user' component = {Userpage} />
-      <Route exact path ='/topic/:id' render={({match}) => <Topic match = {match}/>} />
+      <Route exact path ='/topic/:id' render={({match}) => <AuthThread match = {match}/>} />
       <Route exact path ='/createtopic' component = {CreateTopic}/>
       <Route exact path='/aboutme' component = {AboutMe}/>
     </div>
